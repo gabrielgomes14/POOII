217f1a06c9bc483faaff31652974a9a91f20b450
Gabriel Gomes <91035171+ggg21on@users.noreply.github.com>
2024-07-05 19:42:04 -0300

    Update main.js

--- intro-to-vue-3/main.js
+++ intro-to-vue-3/main.js
@@ -1,13 +1,28 @@
-const { createApp, ref } = Vue;
-
-const app = createApp({
-  setup() {
-    const product = ref("Socks");
-    const image = ref("assets/socks_green.jpg"); // Use / em vez de \
-    const url = ref("https://www.linkedin.com/in/gabriel-gomes-574287258/");
-    
+const app = Vue.createApp({
+data() {
     return {
-      product, image, url
+        cart:0,
+        product: 'Socks',
+        image: './assets/socks_blue.jpg',
+        inStock: true,
+        details: ['50% cotton', '30% wool', '20% polyester'],
+        variants: [
+            { id: 2234, color: 'green', image: './assets/socks_green.jpg' },
+            { id: 2235, color: 'blue', image: './assets/socks_blue.jpg' },
+        ]
     }
-  }
-});
+    
+},
+methods: {
+    addToCart() {
+      this.cart += 1
+    },
+    removeFromCart() {
+        if (this.cart > 0) 
+        this.cart -= 1
+      },
+    updateImage(variantImage) {
+        this.image = variantImage
+      }
+}
+})
